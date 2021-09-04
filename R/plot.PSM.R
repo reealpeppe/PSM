6c7699a1a1f25b1f455175271f8ab9cfe000b693
reealpeppe <58630278+reealpeppe@users.noreply.github.com>
2021-09-04 09:54:02 +0200

    Add files via upload

--- R/plot.PSM.R
+++ R/plot.PSM.R
@@ -2,6 +2,7 @@
 #'
 #' @description There are two plots: in the first one there are the residuals while the second one shows the observed against fitted values
 #' @param object an object of class 'PSM'
+#' @param legend.pos position of the legend. Can be moved according to points position
 #' @param ... further arguments passed to the generic 'plot' function
 #'
 #' @return
@@ -15,16 +16,16 @@
 #' psm <- PartialSplines(y, x, tx)
 #' plot(psm)
 
-plot.PSM <- function(object, ...) {
+plot.PSM <- function(object, legend.pos = 'topleft', ...) {
   y <- object$y
   y.hat <- fitted(object)
   tx <- object$tx
   res <- y - y.hat
-  plot(res, ylab = "Residuals", ...)
+  plot(res, ylab = "Residuals")
   abline(h = 0, col = 2)
   readline(prompt = "Type <Enter> to go to the next plot: ")
   plot(tx, y, main = 'Fitted vs observed values', ...)
   points(tx, y.hat, col = 4, pch = 4)
-  legend("topleft", pch = c(16, 4), col = c(1, 4), legend = c("Observed" ,'Fitted'))
+  legend(legend.pos, pch = c(16, 4), col = c(1, 4), legend = c("Observed" ,'Fitted'))
 
 }
